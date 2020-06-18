70f3295feb061da842ce8c51c2f8383131aefb1a
Sergey-Cherkasov <s.cherkasov_tester@yahoo.com>
2020-06-18 12:23:12 +0700

    Homework finished.

--- WeatherApp/app/src/main/java/br/svcdev/weatherapp/MainFragment.java
+++ WeatherApp/app/src/main/java/br/svcdev/weatherapp/MainFragment.java
@@ -9,14 +9,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import br.svcdev.weatherapp.databinding.FragmentMainBinding;
+
 public class MainFragment extends Fragment {
+
+    FragmentMainBinding mBinding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+mBinding = FragmentMainBinding.inflate(inflater, container,false);
+        mBinding.tvLocationCity.setText(R.string.Kemerovo);
+        return mBinding.getRoot();
+    }
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mBinding = null;
     }
 }
